f9481633764507ce891ff29722124a6acd19977c
SunilSyal <sunil.syal@gmail.com>
2017-03-24 23:29:01 +0000

    q

--- index.js
+++ index.js
@@ -77,30 +77,22 @@ restService.post('/slack-test', function(req, res) {
 restService.post('/fb-test', function(req, res) {
 
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    var messages = [{
-        "payload": {
-            "facebook": {
-                "attachment": {
-                    "type": "",
-                    "payload": {
-                        "template_type": "button",
-                        "text": "What do you want to do next?",
-                        "buttons": [{
-                            "type": "web_url",
-                            "url": "https://login.microsoftonline.com/bluebankb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_BlueBankSUSI&client_Id=0f7ef810-2f9c-424c-942a-48c6ea361d9a&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fgoogle.com%3A44316%2F.auth%2Flogin%2Faad%2Fcallback&scope=openid&response_type=id_token&prompt=login",
-                            "title": "Show Website"
-                        }]
-                    }
-                }
+    var messages = {
+        "attachment": {
+            "type": "audio",
+            "payload": {
+                "url": "https://petersapparel.com/bin/clip.mp3"
             }
         }
-    }];
+    }
 
     return res.json({
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-fb',
-        messages: messages
+        data: {
+            facebook: messages
+        }
     });
 });
 
