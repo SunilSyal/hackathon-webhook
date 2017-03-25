bbe10e2693e4842706d46b8098544e7bcefbc144
SunilSyal <sunil.syal@gmail.com>
2017-03-25 00:14:20 +0000

    q

--- index.js
+++ index.js
@@ -24,13 +24,41 @@ restService.post('/echo', function(req, res) {
                 "payload": {
                     "facebook": {
                         "attachment": {
-                            "type": "image",
+                            "type": "template",
                             "payload": {
-                                "url": "https://petersapparel.com/img/shirt.png"
+                                "template_type": "button",
+                                "text": "What do you want to do next?",
+                                "buttons": [{
+                                        "type": "web_url",
+                                        "url": "https://login.microsoftonline.com/bluebankb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_BlueBankSUSI&client_Id=0f7ef810-2f9c-424c-942a-48c6ea361d9a&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fgoogle.com%3A44316%2F.auth%2Flogin%2Faad%2Fcallback&scope=openid&response_type=id_token&prompt=login",
+                                        "title": "Show Website"
+                                    },
+                                    {
+                                        "type": "postback",
+                                        "title": "Start Chatting",
+                                        "payload": "USER_DEFINED_PAYLOAD"
+                                    }
+                                ]
                             }
                         }
+                    },
+                    "kik": {
+                        "type": "",
+                        "body": ""
+                    },
+                    "slack": {
+                        "text": "",
+                        "attachments": []
+                    },
+                    "telegram": {
+                        "text": ""
+                    },
+                    "viber": {
+                        "type": "text",
+                        "text": ""
                     }
-                }
+                },
+                "type": 4
             },
             {
                 "payload": {
