2bb8f2f274f27b60bf3facf7d58bff026475e597
SunilSyal <sunil.syal@gmail.com>
2017-03-25 00:00:53 +0000

    a

--- index.js
+++ index.js
@@ -16,30 +16,54 @@ restService.post('/echo', function(req, res) {
     return res.json({
         speech: speech,
         source: 'webhook-echo',
-        messages: [{
-            "payload": {
-                "facebook": {
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "button",
-                            "text": "What do you want to do next?",
-                            "buttons": [{
-                                    "type": "web_url",
-                                    "url": "https://login.microsoftonline.com/bluebankb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_BlueBankSUSI&client_Id=0f7ef810-2f9c-424c-942a-48c6ea361d9a&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fgoogle.com%3A44316%2F.auth%2Flogin%2Faad%2Fcallback&scope=openid&response_type=id_token&prompt=login",
-                                    "title": "Show Website"
-                                },
-                                {
-                                    "type": "postback",
-                                    "title": "Start Chatting",
-                                    "payload": "USER_DEFINED_PAYLOAD"
-                                }
-                            ]
-                        }
+        "messages": [
+        {
+          "type": 0,
+          "speech": "hi"
+        },
+        {
+          "payload": {
+            "facebook": {
+              "attachment": {
+                "type": "template",
+                "payload": {
+                  "template_type": "button",
+                  "text": "What do you want to do next?",
+                  "buttons": [
+                    {
+                      "type": "web_url",
+                      "url": "https://login.microsoftonline.com/bluebankb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_BlueBankSUSI&client_Id=0f7ef810-2f9c-424c-942a-48c6ea361d9a&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fgoogle.com%3A44316%2F.auth%2Flogin%2Faad%2Fcallback&scope=openid&response_type=id_token&prompt=login",
+                      "title": "Show Website"
+                    },
+                    {
+                      "type": "postback",
+                      "title": "Start Chatting",
+                      "payload": "USER_DEFINED_PAYLOAD"
                     }
+                  ]
                 }
+              }
+            },
+            "kik": {
+              "type": "",
+              "body": ""
+            },
+            "slack": {
+              "text": "",
+              "attachments": []
+            },
+            "telegram": {
+              "text": ""
+            },
+            "viber": {
+              "type": "text",
+              "text": ""
             }
-        }]
+          },
+          "type": 4
+        }
+      ]
+    },
     });
 });
 
